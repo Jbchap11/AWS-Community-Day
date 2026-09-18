c734fceb4191aeab7c30df4b3931ab626bad3203
Jbchap11 <jiahbhola1104@gmail.com>
2026-09-18 11:38:18 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/AWS-Community-Day/',
+export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
   },
-}))
+})
